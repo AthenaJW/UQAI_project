2535230e1323839cab2b43e99cd41f868c32930d
Athena Wang <wanga@dhcp-10-31-186-15.dyn.MIT.EDU>
2026-03-26 01:01:54 -0400

    limited test set for speed

--- experiments.py
+++ experiments.py
@@ -65,23 +65,23 @@ task_list = [
     #'public_relations', 'management', 'marketing',
 
     # --- Additional College-Level Subjects ---
-    'college_biology', 
-    'college_mathematics', 
-    'college_physics', 
+    #'college_biology', 
+    #'college_mathematics', 
+    #'college_physics', 
 
     # --- Additional High School-Level Subjects ---
-    'high_school_chemistry',
-    'high_school_european_history',
-    'high_school_geography',
-    'high_school_government_and_politics',
-    'high_school_macroeconomics',
-    #'high_school_mathematics',
-    #'high_school_microeconomics',
-    #'high_school_physics',
-    #'high_school_psychology',
-    #'high_school_statistics',
-    #'high_school_us_history',
-    #'high_school_world_history'
+    #'high_school_chemistry',
+    #'high_school_european_history',
+    #'high_school_geography',
+    #'high_school_government_and_politics',
+    #'high_school_macroeconomics',
+    'high_school_mathematics',
+    'high_school_microeconomics',
+    'high_school_physics',
+    'high_school_psychology',
+    'high_school_statistics',
+    'high_school_us_history',
+    'high_school_world_history'
     
     #'virology',
     #'sociology',
